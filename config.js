867264472586c1f86212717069b33c630cbcafb7
WerooDev <107784085+Walter11S@users.noreply.github.com>
2023-02-17 23:18:12 -0600

    update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
+module.exports = {
+  
+  Token: process.env.DISCORD_BOT_TOKEN || "",
+  
+  ClientID: process.env.DISCORD_CLIENT_ID || "",
+  
+  API: process.env.OPENAI_API_KEY || "",
+  
+  ENABLE_DIRECT_MESSAGES: false,
 
+ CONVERSATION_MEMORY_SECONDS: 300,
+
+CONVERSATION_START_PROMP: false,
+
+USE_EMBED: true,
+  
+}
